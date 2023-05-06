5734ef4cc79c8e23edceead4ed95cd2f9113ac55
jcmlg97 <126673138+jcmlg97@users.noreply.github.com>
2023-05-06 14:43:25 +0200

    Add files via upload

---
+++ documentacion/js/searchIndex.js
@@ -0,0 +1,19 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\multiplicacion\u0028\u0029",
+            "name": "multiplicacion",
+            "summary": "Esta\u0020funci\u00F3n\u0020devuelve\u0020el\u0020producto\u0020de\u0020dos\u0020numeros",
+            "url": "namespaces/default.html#function_multiplicacion"
+        },                {
+            "fqsen": "\\saluda\u0028\u0029",
+            "name": "saluda",
+            "summary": "",
+            "url": "namespaces/default.html#function_saluda"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
